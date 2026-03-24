be31e6e40e913219762f54fe24af87a6d7c1d0c0
yuvalm <yuvalmalki2004@gmail.com>
2026-03-24 13:35:28 +0200

    feat: add react-toast for task validation

--- src/component/TasksList.tsx
+++ src/component/TasksList.tsx
@@ -4,6 +4,7 @@ import { DataGrid, type GridColDef } from "@mui/x-data-grid";
 import type { Task } from "../types/tasks";
 import CustomToolbar from "./CustomToolbar";
 import { statusesList } from "../types/statuses";
+import { toast, ToastContainer } from "react-toastify";
 
 const columns: GridColDef<(typeof initialTasks)[number]>[] = [
     { field: "title", headerName: "Title", flex: 1 },
@@ -20,8 +21,10 @@ const columns: GridColDef<(typeof initialTasks)[number]>[] = [
 export default function TasksList() {
     const [tasks, setTasks] = React.useState<Task[]>(initialTasks);
 
-    const AddTask = (task: Task) => {
-        setTasks((prev) => [...prev, task]);
+    const AddTask = (newTask: Task) => {
+        tasks.some((task) => newTask.title === task.title)
+            ? toast.warning("Task with this title already exists")
+            : setTasks((prev) => [...prev, newTask]);
     };
 
     function handleRowUpdate(newRow: Task) {
@@ -34,44 +37,47 @@ export default function TasksList() {
     }
 
     return (
-        <DataGrid
-            rows={tasks}
-            columns={columns}
-            processRowUpdate={handleRowUpdate}
-            initialState={{
-                pagination: {
-                    paginationModel: {
-                        pageSize: 5,
+        <>
+            <ToastContainer />
+            <DataGrid
+                rows={tasks}
+                columns={columns}
+                processRowUpdate={handleRowUpdate}
+                initialState={{
+                    pagination: {
+                        paginationModel: {
+                            pageSize: 5,
+                        },
                     },
-                },
-            }}
-            pageSizeOptions={[5]}
-            disableRowSelectionOnClick
-            slots={{ toolbar: CustomToolbar }}
-            slotProps={{
-                toolbar: {
-                    statusesOptions: statusesList,
-                    addTask: AddTask,
-                },
-            }}
-            showToolbar
-            getRowClassName={(params) =>
-                `super-app-theme--${params.row.status.trim()}`
-            }
-            sx={{
-                "& .MuiDataGrid-columnHeaderTitle": {
-                    fontWeight: "bold",
-                },
-                "& .super-app-theme--Done": {
-                    backgroundColor: "#2ad95e",
-                },
-                "& .super-app-theme--In.progress": {
-                    backgroundColor: "#d9cd2a",
-                },
-                "& .super-app-theme--Not.started": {
-                    backgroundColor: "#2ab6d9",
-                },
-            }}
-        />
+                }}
+                pageSizeOptions={[5]}
+                disableRowSelectionOnClick
+                slots={{ toolbar: CustomToolbar }}
+                slotProps={{
+                    toolbar: {
+                        statusesOptions: statusesList,
+                        addTask: AddTask,
+                    },
+                }}
+                showToolbar
+                getRowClassName={(params) =>
+                    `super-app-theme--${params.row.status.trim()}`
+                }
+                sx={{
+                    "& .MuiDataGrid-columnHeaderTitle": {
+                        fontWeight: "bold",
+                    },
+                    "& .super-app-theme--Done": {
+                        backgroundColor: "#2ad95e",
+                    },
+                    "& .super-app-theme--In.progress": {
+                        backgroundColor: "#d9cd2a",
+                    },
+                    "& .super-app-theme--Not.started": {
+                        backgroundColor: "#2ab6d9",
+                    },
+                }}
+            />
+        </>
     );
 }

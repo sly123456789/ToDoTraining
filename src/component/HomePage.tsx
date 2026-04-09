659cf8ecda7df9ca7a297b2682d760ed02f58891
c672670 <c672670@persi-ubun22-10.CASA.NET>
2026-04-09 15:23:09 +0300

    feat: add box settings to homepage

--- src/component/HomePage.tsx
+++ src/component/HomePage.tsx
@@ -1,11 +1,12 @@
+import { Box } from "@mui/material";
 import "./HomePage.css";
 import TasksList from "./TasksList";
 
 export default function HomePage() {
-    return (
-        <>
-            <h1 className="title">To Do List</h1>
-            <TasksList />
-        </>
-    );
+  return (
+    <Box>
+      <h1 className="title">To Do List</h1>
+      <TasksList />
+    </Box>
+  );
 }

9e9a6d25cb04a353b5ef54e22f3621ce35fa5af8
c672670 <c672670@persi-ubun22-10.CASA.NET>
2026-04-13 10:37:14 +0300

    fix - pr: remove all as uses

--- src/component/TasksListToolBar.tsx
+++ src/component/TasksListToolBar.tsx
@@ -13,11 +13,11 @@ import TextField from "@mui/material/TextField";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 import {
-    ColumnsPanelTrigger,
-    FilterPanelTrigger,
-    Toolbar,
-    ToolbarButton,
-    type ToolbarPropsOverrides,
+  ColumnsPanelTrigger,
+  FilterPanelTrigger,
+  Toolbar,
+  ToolbarButton,
+  type ToolbarPropsOverrides,
 } from "@mui/x-data-grid";
 import * as React from "react";
 import { toast } from "react-toastify";
@@ -26,154 +26,157 @@ import { type Task } from "../types/tasks";
 import { statusesList, type Statuses } from "../types/statuses";
 
 declare module "@mui/x-data-grid" {
-    interface ToolbarPropsOverrides {
-        addTask: (task: Task) => void;
-    }
+  interface ToolbarPropsOverrides {
+    addTask: (task: Task) => void;
+  }
 }
 
-export default function TasksListToolBar({
-    addTask,
-}: ToolbarPropsOverrides) {
-    const [newPanelOpen, setNewPanelOpen] = React.useState(false);
-    const newPanelTriggerRef = React.useRef<HTMLButtonElement>(null);
-    const [isStatusSelected, setIsStatusSelected] = React.useState<boolean>(false);
-    const [selectedStatus, setSelectedStatus] = React.useState<Statuses>(
-        statusesList.length > 0 ? statusesList[0] : "",
-    );
-    const [newTaskTitle, setNewTaskTitle] = React.useState<string>("")
+export default function TasksListToolBar({ addTask }: ToolbarPropsOverrides) {
+  const [newPanelOpen, setNewPanelOpen] = React.useState(false);
+  const newPanelTriggerRef = React.useRef<HTMLButtonElement>(null);
+  const [isStatusSelected, setIsStatusSelected] =
+    React.useState<boolean>(false);
+  const [selectedStatus, setSelectedStatus] = React.useState<Statuses>(
+    statusesList.length > 0 ? statusesList[0] : "",
+  );
+  const [newTaskTitle, setNewTaskTitle] = React.useState<string>("");
 
-    const handleClose = () => {
-        setNewPanelOpen(false);
-    };
+  const handleClose = () => {
+    setNewPanelOpen(false);
+  };
 
-    const handleSubmit = (event: React.SubmitEvent) => {
-        try {
-            event.preventDefault();
+  const handleSubmit = (event: React.SubmitEvent) => {
+    try {
+      event.preventDefault();
 
-            const newTask: Task = {
-                id: uuid(),
-                title: newTaskTitle,
-                status: selectedStatus,
-            };
+      const newTask: Task = {
+        id: uuid(),
+        title: newTaskTitle,
+        status: selectedStatus,
+      };
 
-            addTask(newTask);
-            handleClose();
-        } catch (error) {
-            const errorMessage =
-                error instanceof Error
-                    ? error.message
-                    : "Error when trying to submit new task";
-            toast.error(errorMessage);
-        }
-    };
-
-    const handleKeyDown = (event: React.KeyboardEvent) => {
-        if (event.key === "Escape") {
-            handleClose();
-        }
-    };
-
-    const handleStatusChange = (event: SelectChangeEvent) => {
-        setSelectedStatus(event.target.value);
-    };
-
-    const handleTitleChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-        setNewTaskTitle(event.target.value)
+      addTask(newTask);
+      handleClose();
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error
+          ? error.message
+          : "Error when trying to submit new task";
+      toast.error(errorMessage);
     }
+  };
 
-    return (
-        <Toolbar>
-            <Tooltip title="Add new task">
-                <ToolbarButton
-                    ref={newPanelTriggerRef}
-                    aria-describedby="new-panel"
-                    onClick={() => setNewPanelOpen((prev) => !prev)}>
-                    <AddIcon fontSize="small" />
-                </ToolbarButton>
-            </Tooltip>
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Escape") {
+      handleClose();
+    }
+  };
 
-            <Popper
-                open={newPanelOpen}
-                anchorEl={newPanelTriggerRef.current}
-                placement="bottom-end"
-                id="new-panel"
-                onKeyDown={handleKeyDown}>
-                <ClickAwayListener
-                    onClickAway={() => {
-                        if (isStatusSelected) return;
-                        handleClose();
-                    }}>
-                    <Paper
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: 2,
-                            width: 300,
-                            p: 2,
-                        }}
-                        elevation={8}>
-                        <Typography fontWeight="bold">Add new task</Typography>
-                        <form onSubmit={handleSubmit}>
-                            <Stack spacing={2}>
-                                <TextField
-                                    label="Task title"
-                                    name="title"
-                                    size="small"
-                                    autoFocus
-                                    fullWidth
-                                    required
-                                    onChange={handleTitleChange}
-                                />
-                                <Select
-                                    value={selectedStatus}
-                                    label="Status"
-                                    name="status"
-                                    onOpen={() => setIsStatusSelected(true)}
-                                    MenuProps={{
-                                        TransitionProps: {
-                                            onExited: () =>
-                                                setIsStatusSelected(false),
-                                        },
-                                    }}
-                                    onChange={handleStatusChange}>
-                                    {statusesList.map((option) => (
-                                        <MenuItem key={option} value={option}>
-                                            {option}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                                <Button
-                                    type="submit"
-                                    variant="contained"
-                                    fullWidth>
-                                    Add task
-                                </Button>
-                            </Stack>
-                        </form>
-                    </Paper>
-                </ClickAwayListener>
-            </Popper>
+  const handleStatusChange = (event: SelectChangeEvent<Statuses>) => {
+    setSelectedStatus(event.target.value);
+  };
 
-            <Tooltip title="Columns">
-                <ColumnsPanelTrigger render={<ToolbarButton />}>
-                    <ViewColumnIcon fontSize="small" />
-                </ColumnsPanelTrigger>
-            </Tooltip>
+  const handleTitleChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+  ) => {
+    setNewTaskTitle(event.target.value);
+  };
 
-            <Tooltip title="Filters">
-                <FilterPanelTrigger
-                    render={(props, state) => (
-                        <ToolbarButton {...props} color="default">
-                            <Badge
-                                badgeContent={state.filterCount}
-                                color="primary"
-                                variant="dot">
-                                <FilterListIcon fontSize="small" />
-                            </Badge>
-                        </ToolbarButton>
-                    )}
+  return (
+    <Toolbar>
+      <Tooltip title="Add new task">
+        <ToolbarButton
+          ref={newPanelTriggerRef}
+          aria-describedby="new-panel"
+          onClick={() => setNewPanelOpen((prev) => !prev)}
+        >
+          <AddIcon fontSize="small" />
+        </ToolbarButton>
+      </Tooltip>
+
+      <Popper
+        open={newPanelOpen}
+        anchorEl={newPanelTriggerRef.current}
+        placement="bottom-end"
+        id="new-panel"
+        onKeyDown={handleKeyDown}
+      >
+        <ClickAwayListener
+          onClickAway={() => {
+            if (isStatusSelected) return;
+            handleClose();
+          }}
+        >
+          <Paper
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              width: 300,
+              p: 2,
+            }}
+            elevation={8}
+          >
+            <Typography fontWeight="bold">Add new task</Typography>
+            <form onSubmit={handleSubmit}>
+              <Stack spacing={2}>
+                <TextField
+                  label="Task title"
+                  name="title"
+                  size="small"
+                  autoFocus
+                  fullWidth
+                  required
+                  onChange={handleTitleChange}
                 />
-            </Tooltip>
-        </Toolbar>
-    );
+                <Select
+                  value={selectedStatus}
+                  label="Status"
+                  name="status"
+                  onOpen={() => setIsStatusSelected(true)}
+                  MenuProps={{
+                    TransitionProps: {
+                      onExited: () => setIsStatusSelected(false),
+                    },
+                  }}
+                  onChange={handleStatusChange}
+                >
+                  {statusesList.map((option) => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+                <Button type="submit" variant="contained" fullWidth>
+                  Add task
+                </Button>
+              </Stack>
+            </form>
+          </Paper>
+        </ClickAwayListener>
+      </Popper>
+
+      <Tooltip title="Columns">
+        <ColumnsPanelTrigger render={<ToolbarButton />}>
+          <ViewColumnIcon fontSize="small" />
+        </ColumnsPanelTrigger>
+      </Tooltip>
+
+      <Tooltip title="Filters">
+        <FilterPanelTrigger
+          render={(props, state) => (
+            <ToolbarButton {...props} color="default">
+              <Badge
+                badgeContent={state.filterCount}
+                color="primary"
+                variant="dot"
+              >
+                <FilterListIcon fontSize="small" />
+              </Badge>
+            </ToolbarButton>
+          )}
+        />
+      </Tooltip>
+    </Toolbar>
+  );
 }

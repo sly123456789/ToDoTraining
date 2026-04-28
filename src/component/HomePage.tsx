39b8c92c46d660c31bfb8aacec836c557ed29eb8
c672670 <c672670@persi-ubun22-10.CASA.NET>
2026-04-28 13:00:55 +0300

    feat: move provider to HomePage.tsx

--- src/component/HomePage.tsx
+++ src/component/HomePage.tsx
@@ -1,12 +1,16 @@
 import { Box } from "@mui/material";
 import "./HomePage.css";
 import TasksList from "../features/tasks/TasksList";
+import { store } from "../app/store";
+import { Provider } from "react-redux";
 
 export default function HomePage() {
   return (
     <Box>
-      <h1 className="title">To Do List</h1>
-      <TasksList />
+      <Provider store={store}>
+        <h1 className="title">To Do List</h1>
+        <TasksList />
+      </Provider>
     </Box>
   );
 }
